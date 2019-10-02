07450ea0ffa14991c192216ce79130572ad0dd22
joeymedina <joeymedina@Joeys-MacBook-Pro.local>
2019-10-02 12:55:20 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MeetingsComponent } from './meetings/meetings.component';
+import { FriendsComponent } from './friends/friends.component';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'meetings', component: MeetingsComponent},
+  {path: 'friends', component: FriendsComponent}
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+];
